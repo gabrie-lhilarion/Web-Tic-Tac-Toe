9d2db8b51a296a8be686d5bfeed3680c2f9f69e0
Gabriel Hilarion <gabrielhilarion@gmail.com>
2024-08-22 10:53:48 +0100

    Revert "Gabriel frontend pages"

--- tic-tac-toe/src/main.jsx
+++ tic-tac-toe/src/main.jsx
@@ -1,31 +1,10 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-import Root from "./routes/root";
-import ErrorPage from './error-page';
-import BoardPage from "./routes/board";
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/board",
-    element: <BoardPage />,
-    errorElement: <ErrorPage />,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
