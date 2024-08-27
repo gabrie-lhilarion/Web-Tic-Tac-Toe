4c92b4e49a73b5372dc618d63a9a97198d204937
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-08-27 14:06:21 +0100

    Create controls to select play mode, board size ant start game

--- tic-tac-toe/src/components/StartGameBtn.jsx
+++ tic-tac-toe/src/components/StartGameBtn.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-function StartGameBtn({ setBoard, textInfo }) {
+function StartGameBtn({ playGame, textInfo }) {
     return (
         <button
-            onClick={(e) => setBoard(e.target)}
+            onClick={playGame}
+            id="play"
             className='bg-black text-slate-400 rounded-full p-4 mt-2'
             type="button">
             {textInfo}
--- tic-tac-toe/src/routes/tic-tac-toe.jsx
+++ tic-tac-toe/src/routes/tic-tac-toe.jsx
@@ -4,13 +4,12 @@ import {
     selectPlayMode,
     hideInputElement,
     highlightBoardChoice
-} from '../helpers/game_logic/play_mode'
+} from '../helpers/game_logic/game'
 
 import { SizeThree, SizeFour, SizeFive } from '../components/gameBoard'
 import BoardChoice from '../components/BoardChoice'
 import HeadingTwo from '../components/HeadingTwo'
 import HeadingOne from '../components/HeadingOne'
-import Game from '../helpers/game_logic/game'
 import StartGameBtn from '../components/StartGameBtn'
 
 import Styles from '../helpers/css_classes/classes.json'
@@ -18,17 +17,71 @@ import Text from '../helpers/text/text.json'
 import Opponent from '../components/Opponent'
 
 
-
 function TicTacToe() {
     const [size, setSize] = React.useState(3)
-    const gameData = useLoaderData();
-    console.log(gameData)
+    const [game, setGame] = React.useState({
+        board: 3,
+        playing: null,
+        mode: {
+            type: 1,
+            gameowner: 'me',
+            ai: 'ai'
+        }
+    })
 
-    const playMode = (target) => {
-        if (target.tagName !== 'INPUT') {
+    React.useEffect(() => {
+
+        if (game.playing === true) {
+
+            const inPlay = document.getElementById("in-play")
+            const beforePlay = document.getElementById("before-play")
+            beforePlay.classList.add('hidden')
+            inPlay.classList.remove('hidden')
+        }
+
+    }, [game])
+
+    const gameData = useLoaderData();
+
+    const setPlayMode = (target) => {
+
+        if (target.tagName === 'BUTTON') {
+
+            const friendName = target.parentElement.querySelector('input').value
+            const playBtn = document.getElementById("play")
+            if (friendName.trim().length < 3) {
+                alert("Please enter name at leat 3 characters")
+            } else {
+                const mode = {}
+                const grandParentLi = target.parentElement.parentElement
+                grandParentLi.setAttribute('data-friend', friendName)
+                const playModeChoice = grandParentLi.id
+
+                if (playModeChoice === 'friend') {
+                    mode.type = 2
+                    mode.gameowner = 'me'
+                    mode.friend = friendName
+                }
+
+                if (playModeChoice === 'both') {
+                    mode.type = 3
+                    mode.gameowner = 'me'
+                    mode.ai = 'ai'
+                    mode.friend = friendName
+                }
+
+                setGame({ ...game, mode: mode })
+
+                playBtn.classList.remove("hidden")
+            }
+
+
+        } else {
             selectPlayMode(target)
+            setGame((game) => ({ ...game, mode: { type: 1, gameowner: 'me', ai: 'ai' } }))
             hideInputElement(target)
         }
+
 
     }
 
@@ -36,7 +89,19 @@ function TicTacToe() {
         const choice = el.id
         setSize(Number(choice))
         highlightBoardChoice(el)
+
+        setGame((game) => ({ ...game, board: Number(choice) }))
     }
+
+    const playGame = () => {
+        setGame(game => ({
+            ...game,
+            playing: true
+        }));
+
+
+    };
+
 
 
     return (
@@ -55,7 +120,7 @@ function TicTacToe() {
                 <aside id="sidebar" className={Styles.sideBar}>
                     <div className={Styles.sideBar_inner}>
 
-                        <section className='before_play'>
+                        <section id='before-play' className='before_play p-3'>
                             <HeadingOne
                                 cls={Styles.hero_heading_one}
                                 textInfo={Text.hero}
@@ -80,21 +145,24 @@ function TicTacToe() {
                                     textInfo={'Choose Play Mode'}
                                 />
 
-                                <ul className={Styles.play_mode_ul} onClick={(e) => playMode(e.target)}>
+                                <ul className={Styles.play_mode_ul}>
                                     <Opponent
                                         type={'ai'}
+                                        playMode={setPlayMode}
                                         textInfo={'Play with AI  ☀ '}
                                         buttonText={null}
                                     />
 
                                     <Opponent
                                         type={'friend'}
+                                        playMode={setPlayMode}
                                         textInfo={'Play with a friend ☻'}
                                         buttonText={'Add friend'}
                                     />
 
                                     <Opponent
                                         type={'both'}
+                                        playMode={setPlayMode}
                                         textInfo={'Play with both AI and a friend ☀ && ☻'}
                                         buttonText={'Add friend'}
                                     />
@@ -102,11 +170,32 @@ function TicTacToe() {
                             </div>
 
 
-                            <StartGameBtn textInfo={'Start Game'} />
+                            <StartGameBtn playGame={playGame} textInfo={'Start Game'} />
 
                         </section>
 
-                        <section className='in_play'>
+                        <section id='in-play' className='in_play hidden mt-2 w-[100%]'>
+                            <HeadingOne
+                                cls={Styles.hero_heading_one}
+                                textInfo={'The Game is on now, do your best to win!'}
+                            />
+                            <div className='bg-slate-700 p-6'>
+                                <h2 className='font-extrabold text-2xl'>
+                                    Players
+                                </h2>
+                                <p>
+                                    list of players
+                                </p>
+                            </div>
+
+                            <div className='bg-slate-700 p-6'>
+                                <h2 className='font-extrabold text-2xl'>
+                                    Progress
+                                </h2>
+                                <p>
+                                    <span>0</span> of  <span>9</span>
+                                </p>
+                            </div>
 
                         </section>
                     </div>
